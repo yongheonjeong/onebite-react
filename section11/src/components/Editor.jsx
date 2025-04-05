3cfe4cf744bfab21fcd2aff8c21d824ee6220b23
yongheon <jung09965555@gmail.com>
2025-04-05 14:34:22 +0900

    ch9

---
+++ section11/src/components/Editor.jsx
@@ -0,0 +1,45 @@
+import "./Editor.css"
+import {useState,useRef,useContext} from "react";
+import { TodoDispatchContext } from "../App";
+
+
+const Editor = ()=>{
+
+  const {onCreate} = useContext(TodoDispatchContext);
+  const [content,setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onSubmit  = () => {
+    if(content === ""){
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  }
+
+  const onKeyDown = (e) => {
+      if(e.keyCode === 13){
+        onSubmit();
+      }
+  }
+
+  return (
+      <div className="Editor">
+        <input
+        ref={contentRef}
+        value={content} 
+        onChange={onChangeContent}
+        onKeyDown={onKeyDown}
+        placeholder="새로운 Todo..."/
+        >
+        <button onClick={onSubmit}>추가</button>
+      </div>
+  );
+}
+
+export default Editor;
