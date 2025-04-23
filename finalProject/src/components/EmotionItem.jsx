ea98849d10d59c0d08ef5ce17b81aa72dcbc809f
yongheon Jeong <jung09965555@gmail.com>
2025-04-23 13:45:36 +0900

    12.13 add New.jsx UI

---
+++ finalProject/src/components/EmotionItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {getEmotionImage} from '../util/get-emotion-image'
+import './EmotionItem.css'
+
+const EmotionItem = ({emotionId,emotionName,isSelected})=>{
+    return <div className={`EmotionItem ${isSelected? `EmotionItem_on_${emotionId}`: ''}`}>
+        <img className='emotion_img' src={getEmotionImage(emotionId)}/>
+        <div className='emotion_name'>{emotionName}</div>
+    </div>
+}
+
+export default EmotionItem;
--- finalProject/src/pages/New.jsx
+++ finalProject/src/pages/New.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Editor from '../components/Editor'
 
 const New = () => {
-    return <div>New</div>
+    return (
+        <div>
+            <Header title={"새 일기 쓰기"} leftChild={<Button text={"< 뒤로가기"}/>}/>
+            <Editor />
+        </div>
+    )
 }
 
 export default New;
