501932f3cfd982a07a22addf32b222d22cba3768
yongheon Jeong <jung09965555@gmail.com>
2025-04-23 19:37:14 +0900

    12.16 Viewer UI finish

---
+++ finalProject/src/components/Viewer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import emotionList from '../util/constants';
+import { getEmotionImage } from '../util/get-emotion-image';
+import './Viewer.css'
+
+const Viewer = ()=>{
+    const emotionId = 2;
+
+    const emotionItem = emotionList.find((item)=> String(item.emotionId) === String(emotionId));
+
+    return <div className='Viewer'>
+        <section className='img_section'>
+        <h4>오늘의 감정</h4>
+        <div className={`emotion_img_wrapper emotion_img_wrapper_${emotionId}`}>
+            <img src={getEmotionImage(emotionId)} />
+            <div>{emotionItem.emotionName}</div>
+        </div>                    
+        </section>
+        <section className='content_section'>
+            <h4>오늘의 일기</h4>
+            <div className='content_wrapper'>
+                <p>일기 ....</p>
+            </div>
+        </section>
+    </div>
+}
+
+export default Viewer;
